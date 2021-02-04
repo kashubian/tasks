71d3b8b159dc55612a8b4ab44b72f3c6ffb19666
michal <karalusmichal@gmail.com>
2021-02-04 21:09:19 +0100

    User can see only his/her tasks

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,15 +1,21 @@
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Task
 
 
-class TasksListView(generic.ListView):
+class TasksListView(LoginRequiredMixin, generic.ListView):
 
     model = Task
     ordering = ['date']
     context_object_name = 'tasks'
     template_name = 'tasks/list.html'
 
+    def get_queryset(self):
+        user = self.request.user
+        tasks = Task.objects.filter(user=user)
+        return tasks
+    
 
 class TaskDetailView(generic.DetailView):
 
--- project/settings.py
+++ project/settings.py
@@ -116,6 +116,8 @@ LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
 
+LOGIN_URL = 'login/'
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = env('EMAIL_HOST')
