b26a08cd7277e5c667cbfa55b03663ac6cb4e580
michal <karalusmichal@gmail.com>
2021-02-06 18:48:38 +0100

    Add calendar widget

--- apps/tasks/forms.py
+++ apps/tasks/forms.py
@@ -3,6 +3,16 @@ from .models import Task
 
 
 class TaskCreateForm(forms.ModelForm):
+    date = forms.DateTimeField(
+        input_formats = ['%Y-%m-%dT%H:%M'], 
+        widget = forms.DateTimeInput(
+        attrs={
+            'type': 'datetime-local',
+            'class': 'form-control'
+            },
+        format='%Y-%m-%dT%H:%M')
+        )
+
     class Meta:
         model = Task
         fields = [
